f042553423f83573d6275cb474846cf2729c64a9
Diego Bernardo <diegobsiufrpe@gmail.com>
2024-09-26 22:07:14 -0300

    contagem-em-algoritmos

---
+++ contagem-em-algoritmos/3.1.py
@@ -0,0 +1,17 @@
+k = 0
+
+for m in range(4, 11):
+    k += 2
+print(k)
+
+for n in range(7, 16):
+    k -= 1
+print(k)
+
+for p in range(101, 121):
+    k += 1
+print(k)
+
+for q in range(57, 64):
+    k -= 1
+print(k)
---
+++ contagem-em-algoritmos/3.2.py
@@ -0,0 +1,9 @@
+k = 0
+
+for m in range(4, 11):
+    for n in range(7, 16):
+        for p in range(101, 121):
+            for q in range(57, 64):
+                k += 1
+
+print(k)
